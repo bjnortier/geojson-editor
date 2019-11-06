b1e40161eb9f37e8237af198728c65e3f04a5972
Ben Nortier <ben@bjnortier.com>
2019-11-06 14:57:08 +0200

    Initial app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index = () => (
+  <div>I am an editor</div>
+)
+
+export default Index
---
+++ test/functional/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Editor from '../../src/'
+
+render(
+  <Editor />,
+  document.getElementById('root'))
